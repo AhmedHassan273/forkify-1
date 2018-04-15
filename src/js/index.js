ad13f634091372c4b30cc4c4907cacd0b539ac79
Aivirth <aivirth.work@gmail.com>
2018-04-15 20:05:34 +0200

    Installed axios and tried data fetching from food2fork api

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = '96c0f156a634b7a19acd17bd9684e913';
+  //error handling
+  try {
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes)
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+getResults('pizza');
