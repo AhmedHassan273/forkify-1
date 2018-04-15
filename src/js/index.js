11549a21125f9db93de8991efb3dbe5cd1401e2c
Aivirth <aivirth.work@gmail.com>
2018-04-15 20:15:49 +0200

    Moved getResults to Search class

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,7 @@
-import axios from 'axios';
+import Search from "./models/Search";
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = '96c0f156a634b7a19acd17bd9684e913';
-  //error handling
-  try {
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes)
-  } catch (err) {
-    console.log(err);
-  }
+const search = new Search("pizza");
 
-}
+console.log(search);
 
-getResults('pizza');
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,3 +1,22 @@
-export default {
-  text: "test"
-};
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this._query = query;
+  }
+
+  async getResults() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = "96c0f156a634b7a19acd17bd9684e913";
+    //error handling
+    try {
+      const res = await axios(
+        `${proxy}http://food2fork.com/api/search?key=${key}&q=${this._query}`
+      );
+      this._result = res.data.recipes;
+      console.log(this._result);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
