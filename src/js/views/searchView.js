b7c9d964f8f82f5ea24f8f4252d272488727ad2d
Aivirth <aivirth.work@gmail.com>
2018-04-25 22:48:03 +0200

    Added recipe title shortener

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,8 +1,29 @@
 import { elements } from "./base";
+
 export const getInput = () => elements.searchInput.value;
 export const clearInput = () => (elements.searchInput.value = "");
 export const clearResults = () => (elements.searchResList.innerHTML = "");
 
+/*
+// 'Pasta with tomato and spinach'
+acc: 0 | acc + cur.length = 5 | newTitle = ['Pasta']
+acc: 5 | acc + cur.length = 9 | newTitle = ['Pasta', 'with']
+acc: 9 | acc + cur.length = 15 | newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 | acc + cur.length = 18 | newTitle = ['Pasta', 'with', 'tomato']
+*/
+
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+  }
+  return title;
+};
 const renderRecipe = recipe => {
   const markup = `
     <li>
